e102f3e28efc1a979568622a49fa2f8f86a8bb85
longhairy <londo112@hotmail.com>
2023-05-17 17:50:08 +0200

    Cleaning added

---
+++ EASV Test Exam Console/Cleaning.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EASV_Test_Exam_Console
+{
+    internal class Cleaning
+    {
+        private int area { get; set; }
+        private bool IsDeluxe { get; set; }
+        private bool IsDoubleWash { get; set; }
+        private bool IsImpreg {get; set; }
+        public Cleaning(int area, bool IsDeluxe, bool IsDoubleWash,bool IsImpreg) 
+        {
+            this.area = area;
+            if(IsDeluxe)
+            {
+                this.IsDeluxe = IsDeluxe;
+                this.IsDoubleWash = false;
+                this.IsImpreg = false;
+            }
+            else
+            {
+                this.IsDoubleWash = IsDoubleWash;
+                this.IsImpreg = IsImpreg;
+            }
+        }
+    }
+}
