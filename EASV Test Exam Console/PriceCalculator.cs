59048295cbda97c2e52acf8e6d219db9303956fc
longhairy <londo112@hotmail.com>
2023-05-18 10:36:48 +0200

    priceCalculator added (need implementation)

---
+++ EASV Test Exam Console/PriceCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EASV_Test_Exam_Console
+{
+    internal class PriceCalculator
+    {
+        public int CalculateCleaningPrice(Cleaning cleaning)
+        {
+            return 0;
+        }
+    }
+}
