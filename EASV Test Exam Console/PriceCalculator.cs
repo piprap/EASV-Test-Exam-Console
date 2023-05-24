02de9bf627c37609a35872776c51fca28d709c2d
longhairy <londo112@hotmail.com>
2023-05-24 12:51:11 +0200

    CalculateCleaning split in 3 methods

--- EASV Test Exam Console/PriceCalculator.cs
+++ EASV Test Exam Console/PriceCalculator.cs
@@ -13,34 +13,49 @@ namespace EASV_Test_Exam_Console
             if (cleaning.area<0) throw new ArgumentException("Area cannot be negative");
             if (cleaning.area>500) throw new ArgumentException("For Areas Highter than 500 Call for price");
             int TotalPrice = 500; //starting fee
-            int area = cleaning.area;
-            if (area <= 100)
-            {
-                TotalPrice += area * 20;
-            }
-            else if (area <= 200)
-            {
-                TotalPrice += (area - 100) * 15;
-                TotalPrice += 2000;
-            }
-            else if (area <= 500)
-            {
-                TotalPrice += (area - 200) * 10; // the + was missing but tests was used and located because of testcases 9, 10 and 11
-                TotalPrice += 3500;
-            }
+            
+            TotalPrice += CalculateAreaPrice(cleaning.area);
+            TotalPrice = (int)Math.Round(TotalPrice * CalculateMultiplier(cleaning));
+
+
+            return TotalPrice;
+        }
+
+        private double CalculateMultiplier(Cleaning cleaning)
+        {
             if (cleaning.IsDeluxe || cleaning.IsDoubleWash && cleaning.IsImpreg)
             {
-                TotalPrice = (int)Math.Round(TotalPrice * 1.6);
+                return 1.6;
             }
             else if (cleaning.IsDoubleWash)
             {
-                TotalPrice = (int)Math.Round(TotalPrice * 1.5);
+                return 1.5;
             }
             else if (cleaning.IsImpreg)
             {
-                TotalPrice = (int)Math.Round(TotalPrice * 1.2);
+                return 1.2;
             }
-            return TotalPrice;
+            return 1;
+        }
+
+        private int CalculateAreaPrice(int area)
+        {
+            int AreaPrice=0;
+            if (area <= 100)
+            {
+                AreaPrice += area * 20;
+            }
+            else if (area <= 200)
+            {
+                AreaPrice += (area - 100) * 15;
+                AreaPrice += 2000;
+            }
+            else if (area <= 500)
+            {
+                AreaPrice += (area - 200) * 10; // the + was missing but tests was used and located because of testcases 9, 10 and 11
+                AreaPrice += 3500;
+            }
+            return AreaPrice;
         }
     }
 }
